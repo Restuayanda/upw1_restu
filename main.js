8941f947c8286b3204d45a6bf701f50506ee4fe7
Restuayanda <136540889+Restuayanda@users.noreply.github.com>
2023-06-14 09:35:23 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let nama = 'zidni mubarok' //variabel tipe string
+let usia = '16' //variable tipe integer (number)
+let tnggiBadan ='168' //variable tipe double float
+let beratBadan ='56' //tipe string 
+let status = null
+alert(
+    'nama saya ${nama} usia saya itu ${usia} tinggi badan ${tinggiBadan}cm Berat badan saya adalah ${beratBadan}kg dan status saya adalah ${status}',
+    )
